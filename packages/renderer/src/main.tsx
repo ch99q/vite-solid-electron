0f9cc6774cd2429a49710dafeba2ba0c193487b2
Christian Hansen <64793a1a@gmail.com>
2022-02-18 13:45:48 +0100

    Added hash routing for solid-app-router with electron bug-fix

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -5,8 +5,33 @@ import "./samples/electron-store";
 
 import { onMount } from "solid-js";
 import { render } from "solid-js/web";
-import { Router } from "solid-app-router";
+import { Router, createIntegration } from "solid-app-router";
 import App from "./app";
+
+function bindEvent(target: EventTarget, type: string, handler: EventListener) {
+  target.addEventListener(type, handler);
+  return () => target.removeEventListener(type, handler);
+}
+
+function electronIntegration() {
+  return createIntegration(
+    () => window.location.hash.slice(1),
+    ({ value, scroll }) => {
+      if (value.includes("index.html#")) {
+        value = new URL("file://" + value).hash;
+      }
+      window.location.hash = value.startsWith("/#/") ? value.slice(2) : value;
+      if (scroll) {
+        window.scrollTo(0, 0);
+      }
+    },
+    (notify) => bindEvent(window, "hashchange", () => notify()),
+    {
+      go: (delta) => window.history.go(delta),
+      renderPath: (path) => `#${path}`,
+    }
+  );
+}
 
 render(() => {
   onMount(() => {
@@ -14,7 +39,7 @@ render(() => {
   });
 
   return (
-    <Router>
+    <Router source={electronIntegration()}>
       <App />
     </Router>
   );
